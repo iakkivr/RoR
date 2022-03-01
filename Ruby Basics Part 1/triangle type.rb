ebf7753bae3ed080c42754ea8822fdd6fd33f327
iakkivr <smolskyi.vitalii@gmail.com>
2022-03-01 16:05:43 +0300

    Lesson 1 with edits v2

--- Ruby Basics Part 1/triangle type.rb
+++ Ruby Basics Part 1/triangle type.rb
@@ -10,12 +10,12 @@
 puts "Введи 3 стороны треугольника через пробел"
 triangle_sides = gets.chomp.split(" ").map(&:to_i).sort
 # result = "Треугольник не является равносторонним, равнобедренным или прямоугольным"
-result =  if triangle_sides[0] == triangle_sides[1] || triangle_sides[1] == triangle_sides[2]
-            "Равнобедренный"
+result =  if (triangle_sides[0]**2  + triangle_sides[1]**2) == triangle_sides[2]**2
+            "Прямоугольный"
           elsif triangle_sides[0] == triangle_sides[1] && triangle_sides[0] == triangle_sides[2]
             "Равносторонний"
-          elsif (triangle_sides[0]**2  + triangle_sides[1]**2) == triangle_sides[2]**2
-            "Прямоугольный"
+          elsif triangle_sides[0] == triangle_sides[1] || triangle_sides[1] == triangle_sides[2]
+            "Равнобедренный"
           else
             "Треугольник не является равносторонним, равнобедренным или прямоугольным"
           end
