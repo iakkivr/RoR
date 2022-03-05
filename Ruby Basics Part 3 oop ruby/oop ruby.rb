dcf7a2c3da465644d6c9703136811f45d097bebb
iakkivr <smolskyi.vitalii@gmail.com>
2022-03-06 01:43:16 +0300

    add classes Train, Route

--- Ruby Basics Part 3 oop ruby/oop ruby.rb
+++ Ruby Basics Part 3 oop ruby/oop ruby.rb
@@ -16,7 +16,7 @@
 class Train
   #  attr_accessor
   attr_reader :number, :type, :amount_of_wagons, :speed, :nearby_station, :route
-# attr_writer
+  # attr_writer
   def initialize(number, type = 1, amount_of_wagons = 1)
     @number = number
     @type = type
@@ -26,46 +26,58 @@ class Train
   def set_speed=(speed)
     @speed = speed
   end
+
   def stop
     @speed = 0
   end
+
   def unhook_wagon
-    @amount_of_wagons -= 1  if @amount_of_wagons > 1 && @speed == 0
+    @amount_of_wagons -= 1  if @amount_of_wagons > 1 && @speed.zero?
   end
+
   def attach_wagon
-    @amount_of_wagons += 1 if  @speed == 0
+    @amount_of_wagons += 1 if  @speed.zero?
   end
 
   def move_to_next_station
-    @index_station += 1
+    @index_station += 1 if @index_station <=
   end
+
   def move_to_previous_station
     @index_station -= 1
   end
 
   def get_route(route)
-    @route = route.list_station
-    @index_station = 0
+    @route = route.get_stations
   end
 
   def nearby_station
-     @nearby_station = { "Предыдущая" => @route[@index_station - 1 ],"Текущая" => @route[@index_station],"Следующая" =>@route[@index_station + 1] }
+     @nearby_station = { "Предыдущая" => @route[@index_station - 1 ],
+                         "Текущая" => @route[@index_station],
+                         "Следующая" =>@route[@index_station + 1] }
   end
+
 end
 
 class Route
   # attr_accessor :route_station
-  attr_reader :list_station
+
   def initialize(begin_station,end_station)
     @list_station = [begin_station,end_station]
   end
+
   def add_station(station)
-    @route_station.insert(1,station)
+    @list_station.insert(1,station)
+  end
+
+  def get_stations
+    @list_station
   end
 
   def remove_station(index_station)
     @list_station.delete_at(index_station) if index_station.positive? && index_station < @list_station.count - 1
   end
+
 end
 
 train = Train.new("First", 1, 10)
