950991ddcba2bdd4f70b540c6d7dce52a49d39bb
iakkivr <smolskyi.vitalii@gmail.com>
2022-03-08 01:12:05 +0300

    create classes train, route and station and interaction between them

--- Ruby Basics Part 3 oop ruby/oop ruby.rb
+++ Ruby Basics Part 3 oop ruby/oop ruby.rb
@@ -1,30 +1,21 @@
-=begin
-Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные
- указываются при создании экземпляра класса
-Может набирать скорость
-Может возвращать текущую скорость
-Может тормозить (сбрасывать скорость до нуля)
-Может возвращать количество вагонов
-Может прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает или уменьшает
-количество вагонов). Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
-Может принимать маршрут следования (объект класса Route).
-При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
-Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только
-на 1 станцию за раз.
-Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
-=end
 class Train
-  #  attr_accessor
-  attr_reader :number, :type, :amount_of_wagons, :speed, :nearby_station, :route
-  # attr_writer
+  attr_reader :number, :type, :amount_of_wagons, :nearby_station, :route, :type, :current_station
+
   def initialize(number, type = 1, amount_of_wagons = 1)
     @number = number
     @type = type
     @amount_of_wagons = amount_of_wagons
+    @speed = 0
+    @direction_of_travel = 1 # 1 - вперед, 0 - назад
+    @route = []
   end
 
-  def set_speed=(speed)
+  def set_speed(speed)
     @speed = speed
+  end
+
+  def current_speed
+    @speed
   end
 
   def stop
@@ -39,48 +30,110 @@ class Train
     @amount_of_wagons += 1 if  @speed.zero?
   end
 
+  def get_route(route, set = 1)
+    @route = route
+    set_station(@route.begin_station) if set.positive?
+  end
+
   def move_to_next_station
-    @index_station += 1 if @index_station <=
+    index_next_station = @route.get_stations.index(current_station) + 1
+    set_station(@route.get_stations[index_next_station])
   end
 
   def move_to_previous_station
-    @index_station -= 1
-  end
-
-  def get_route(route)
-    @route = route.get_stations
+    index_previous_station = @route.get_stations.index(current_station) - 1
+    set_station(@route.get_stations[index_previous_station])
   end
 
   def nearby_station
-     @nearby_station = { "Предыдущая" => @route[@index_station - 1 ],
-                         "Текущая" => @route[@index_station],
-                         "Следующая" =>@route[@index_station + 1] }
+     @nearby_station
   end
+
+  def send(train)
+    @direction_of_travel = 1 ? train.move_to_next_station : train.move_to_previous_station
+  end
+
+  def update_routes
+
+  end
+
+  def set_station(station)
+    @current_station.delete_train(self) unless @current_station.nil?
+    @current_station = station
+    station.take_train(self)
+  end
+
 
 end
 
 class Route
-  # attr_accessor :route_station
 
   def initialize(begin_station,end_station)
     @list_station = [begin_station,end_station]
   end
 
-  def add_station(station)
-    @list_station.insert(1,station)
+  def begin_station
+    @list_station.first
+  end
+
+  def add_station(station,index)
+    index = @list_station.count - 1 if index > @list_station.count - 2
+    index = 1 if index < 1
+    puts @list_station.count
+    @list_station.insert(index,station)
+    #update_routes_for_trains(0)
   end
 
   def get_stations
+    # @list_station.map(&:name)
     @list_station
   end
 
   def remove_station(index_station)
     @list_station.delete_at(index_station) if index_station.positive? && index_station < @list_station.count - 1
+    update_routes_for_trains(1)
   end
 
+  private
+
+  def update_routes_for_trains(set_station = 0)
+    @list_station.each do |station|
+      train.get_route(self,set_station)
+    end
+  end
 end
 
-train = Train.new("First", 1, 10)
-route_first = Route.new('First','Last')
-train.get_route(route_first)
-puts train.nearby_station
+class Station
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @train_list = []
+  end
+
+  def take_train(train)
+    @train_list << train
+  end
+
+  def trains_at_station
+    @train_list.map(&:number)
+  end
+
+  def amount_trains_by_type
+    "Грузовых: #{@train_list.count { |elem| elem.type == 1 }} шт
+Пассажирских: #{@train_list.count { |elem| elem.type == 2 }} шт"
+  end
+
+  def send_train(train)
+    train.send(train)
+    delete_train(train)
+  end
+
+  def delete_train(train)
+    @train_list.delete(train)
+  end
+end
+
+
+
