6fdac4c076233e272e5919bc4d3e4028f6bda157
iakkivr <smolskyi.vitalii@gmail.com>
2022-03-28 21:40:31 +0300

    refactor module InstanceCounter

--- Ruby Basics Part 4 Interfaces, inheritance and polymorphism/instance_counter.rb
+++ Ruby Basics Part 4 Interfaces, inheritance and polymorphism/instance_counter.rb
@@ -1,24 +1,20 @@
 module InstanceCounter
-
   def self.included(base)
-    base.instance_variable_set :@instances, 0
     base.extend ClassMethods
     base.include InstanceMethods
   end
 
   module ClassMethods
-    def instances
-      self.instance_variable_get :@instances
-    end
+    attr_writer :instances
 
+    def instances
+      @instances ||= 0
+    end
   end
 
   module InstanceMethods
-    protected
-
     def register_instance
-      self.class.instance_variable_set :@instances, (self.class.instance_variable_get(:@instances) + 1)
+      self.class.instances += 1
     end
   end
 end
-
